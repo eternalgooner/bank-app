d2bf7abadc42e30332cec67a593134ff39ee8323
David Mackessy <eternalgooner@hotmail.com>
2018-04-22 19:58:42 +0100

    Customer added

--- src/main/java/com/bankapp/model/Customer.java
+++ src/main/java/com/bankapp/model/Customer.java
@@ -1,5 +1,111 @@
 package com.bankapp.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
 
+	private String name;
+	private String address;
+	private String email;
+	private int pin;
+	private List<Account> accountList;
+	
+	public Customer(String name, String address, String email, int pin) {
+		this.name = name;
+		this.address = address;
+		this.email = email;
+		this.pin = pin;
+		this.accountList = new ArrayList<>();
+	}
+	
+	public boolean createAccount(String accountType) {
+		try {
+			//try & create new account & add to DB
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public boolean deleteAccount(int accountId) {
+		try {
+			//try & delete account from DB
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public boolean makeTransfer(int accountFromId, int accountToId, double amount) {
+		try {
+			//try & deduct amount from accountFromId
+			//if successful, then add amount to accountToId
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+	}
+	
+	public boolean makeWithdrawal(int accountFromId, double amount) {
+		try {
+			//try & deduct amount from accountFromId
+			
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+	}
+	
+
+	public double getBlanace(int accountId) {
+		//return balance from DB
+		return 22.3;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public int getPin() {
+		return pin;
+	}
+	
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+
+	public List<Account> getAccountList() {
+		return accountList;
+	}
+
+	public void setAccountList(List<Account> accountList) {
+		this.accountList = accountList;
+	}	
 }
--- src/main/java/com/bankapp/model/Transaction.java
+++ src/main/java/com/bankapp/model/Transaction.java
@@ -4,21 +4,7 @@ import java.util.List;
 
 public class Transaction {
 	
-	private List<Customer> customerList;
-	private boolean customerLoggedIn;
 	
-	public List<Customer> getCustomerList() {
-		return customerList;
-	}
-	public void setCustomerList(List<Customer> customerList) {
-		this.customerList = customerList;
-	}
-	public boolean isCustomerLoggedIn() {
-		return customerLoggedIn;
-	}
-	public void setCustomerLoggedIn(boolean customerLoggedIn) {
-		this.customerLoggedIn = customerLoggedIn;
-	}
 	
 	
 
